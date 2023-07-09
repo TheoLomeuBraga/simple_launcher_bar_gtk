1ce8600a1c0a4d6b4fb407acfb71e26f817e51df
TheoLomeuBraga <theolomeu@gmail.com>
2023-07-09 13:53:40 -0300

    a

--- simple_launcher_bar_gtk.py
+++ simple_launcher_bar_gtk.py
@@ -5,17 +5,47 @@ import os
 from simple_launcher_bar_gtk_config import *
 
 gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+from gi.repository import GLib, Gtk, Adw,Gio
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if side == Sides.LEFT or side == Sides.RIGHT:
+            self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        elif side == Sides.DOWN or side == Sides.TOP:
+            self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+        self.set_child(self.box)
+
+        self.buttons = []
+
+        for l in launchers:
+            button = Gtk.Button(label="Hello")
+            
+            self.box.append(button)
+            
+            button.set_icon_name(l.icon)
+
+            button.set_size_request(size, size)
+            
+
+            self.buttons.append(button)
+
+        
+        
 
 
-class LauncherBar(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
+        # Things will go here
 
-    def do_activate(self):
-        window = Gtk.ApplicationWindow(application=self, title="Hello World")
-        window.present()
+class LauncherBar(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
 
 
 app = LauncherBar()
--- simple_launcher_bar_gtk_config.py
+++ simple_launcher_bar_gtk_config.py
@@ -2,6 +2,8 @@ from simple_launcher_bar_gtk_definitions import *
 
 side = Sides.LEFT
 
+size = 64
+
 launchers = [
     Launcher("firefox","firefox","firefox"),
     Launcher("firefox2","firefox","firefox"),
